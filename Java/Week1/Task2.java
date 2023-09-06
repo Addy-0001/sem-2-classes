6af7666c77ee32ab06e1d77a3ce230fd27f3c5c9
Adamya <adamsneupane@gmail.com>
2023-09-06 15:09:03 +0545

    added week 1 tasks

---
+++ Java/Week1/Task2.java
@@ -0,0 +1,7 @@
+package Week1;
+
+public class Task2 {
+    public static void main(String[] args) {
+        System.out.println("hello world,\nThis is my first program\nI am on module ST4003CEM");
+    }
+}
---
+++ Java/Week1/Task3.java
@@ -0,0 +1,28 @@
+package Week1;
+
+public class Task3 {
+    // A"quoted"String is
+
+    // 'much'better if
+    // you learn
+
+    // the rules of"escape sequences."
+
+    // Also,""
+    // represents an
+    // empty
+
+    // String.Don'tforget:use\"
+
+    // instead of" !
+
+    // ''
+    // is not
+    // the same as"
+
+    public static void main(String[] args) {
+        System.out.println(
+                "A \"quoted\"String is\n\n'much' better if \nyou learn \n\nthe rules of\"escape sequences.\"\n\nAlso, \"\"\nrepresents an\nempty\n\nString.Don'tforget:use\\\" \ninstead of\" '' is not \nthe same as\"");
+    }
+
+}
