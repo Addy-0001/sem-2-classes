868cd668717ab77a65dd1ae8db7518fae26814a1
Adamya <adamsneupane@gmail.com>
2023-09-19 17:32:40 +0545

    optimized code.

--- Object Oriented Programming/Week4/Pyramid.java
+++ Object Oriented Programming/Week4/Pyramid.java
@@ -3,8 +3,8 @@ package Week4;
 public class Pyramid {
     public static void main(String[] args) {
         int star = 1;
-        for (int gap = 5; gap >= 0; gap--) {
-            for (int lineGap = 0; lineGap <= gap; lineGap++) {
+        for (int lineNo = 5; lineNo >= 0; lineNo--) {
+            for (int lineGap = 0; lineGap <= lineNo; lineGap++) {
                 System.out.print(" ");
             }
             for (int lineStar = 1; lineStar <= star; lineStar++) {
--- Object Oriented Programming/Week4/pyramid.py
+++ Object Oriented Programming/Week4/pyramid.py
@@ -1,6 +1,6 @@
 space = 5 
 stars = 1
-for i in range(4, -1, -1):
+for i in range(5, -1, -1):
     print(" "*i, end="")
     print("*"*stars)
     stars += 2
