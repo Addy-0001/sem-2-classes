ef2ced050ec401f5df516ed4cf384b30a9efd09a
Adamya <adamsneupane@gmail.com>
2023-09-19 17:27:50 +0545

    Making a pyramid with python vs java

--- Object Oriented Programming/Week4/Pyramid.java
+++ Object Oriented Programming/Week4/Pyramid.java
@@ -2,12 +2,16 @@ package Week4;
 
 public class Pyramid {
     public static void main(String[] args) {
+        int star = 1;
         for (int gap = 5; gap >= 0; gap--) {
-            System.out.print(" ");
-            for (int stars = 1; stars <= 9; stars++) {
-                System.out.println("*");
-                stars++;
+            for (int lineGap = 0; lineGap <= gap; lineGap++) {
+                System.out.print(" ");
             }
+            for (int lineStar = 1; lineStar <= star; lineStar++) {
+                System.out.print("*");
+            }
+            star = star + 2; 
+            System.out.println("");
         }
     }
 }
---
+++ Object Oriented Programming/Week4/pyramid.py
@@ -0,0 +1,6 @@
+space = 5 
+stars = 1
+for i in range(4, -1, -1):
+    print(" "*i, end="")
+    print("*"*stars)
+    stars += 2
