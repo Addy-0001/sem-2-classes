a3da90a4d6d8e8deebebd110025e2f9dc273f722
Adamya <adamsneupane@gmail.com>
2023-10-09 11:44:20 +0545

    OOP concepts start

---
+++ Object Oriented Programming/OOP/OOPFirst.java
@@ -0,0 +1,34 @@
+package OOP;
+
+public class OOPFirst {
+    public static void main(String[] args) {
+        Person p1 = new Person();
+        p1.first_name = "Adamya";
+        p1.last_name = "Neupane";
+        p1.age = 22;
+        p1.location = "Kaushaltar";
+
+        p1.description(args);
+        p1.ageCalculator();
+    }
+}
+
+class Person {
+    String first_name;
+    String last_name;
+    int age;
+    String location;
+
+    void description(String[] args) {
+        System.out.println("Hi, my name is: " + first_name + " " + last_name);
+        System.out.println("I live in: " + location);
+    }
+
+    void ageCalculator() {
+        if (age > 18) {
+            System.out.println("Good");
+        } else {
+            System.out.println("Underage");
+        }
+    }
+}
