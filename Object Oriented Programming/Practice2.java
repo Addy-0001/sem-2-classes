07fa548a477b7642a5cb7ff5a3ab81216664eec6
Adamya Neupane <adamsneupane@gmail.com>
2024-01-11 07:36:25 +0545

    made calculator work

--- Object Oriented Programming/Practice2.java
+++ Object Oriented Programming/Practice2.java
@@ -1,3 +1,28 @@
+// Making a scientific calculator in java
+
 public class Practice2 {
-    
+    public static void main(String[] args) {
+
+    }
+}
+
+class BaseCalculator {
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public double multiply(double a, double b) {
+        return a * b;
+    }
+}
+
+class AdvancedCalculator extends BaseCalculator {
+    public int multiply(int a, int b, int c) {
+        return a * b * c;
+    }
+
+    public double multiply(double a, double b, double c) {
+        return a * b * c;
+    }
+
 }
